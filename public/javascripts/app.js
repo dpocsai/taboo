1fd8a72be8693b119be0cf3606789c0c57908883
David Pocsai <4122dpocsai@gmail.com>
2021-10-26 15:05:03 -0400

    day1

---
+++ public/javascripts/app.js
@@ -0,0 +1,6 @@
+const changeTime = (time) => {
+  document.querySelector("#time").value = time;
+};
+const changeTeams = (teams) => {
+  document.querySelector("#teams").value = teams;
+};
--- taboo.js
+++ taboo.js
@@ -1,16 +1,45 @@
 const express = require("express");
 const morgan = require("morgan");
+const flash = require("express-flash");
+const session = require("express-session");
+const { body, validationResult } = require("express-validator");
+const store = require("connect-loki");
 
-const app = express();
-const port = process.env.PORT || "8000";
+const app = express(); //sets up the express application object
+const port = process.env.PORT || "8000"; //what port to listen on
+const LokiStore = store(session);
 
-app.set("views", "./views");
-app.set("view engine", "pug");
-app.use(morgan("common"));
+app.set("views", "./views"); //where to find view templates
+app.set("view engine", "pug"); //what view engine to use
+
+app.use(morgan("common")); //enable logging
+app.use(express.static("public")); //find static files in public folder
+app.use(express.urlencoded({ extended: false })); //format to receive data from post requests - incoming request object will be in this format (can also use express.json())
+app.use(
+  session({
+    cookie: {
+      httpOnly: true,
+      maxAge: 31 * 24 * 60 * 60 * 1000, // 31 days in millseconds
+      path: "/",
+      secure: false,
+    },
+    name: "taboo-session-id",
+    resave: false,
+    saveUninitialized: true,
+    secret: "this is not very secure",
+    store: new LokiStore({}),
+  })
+);
+app.use(flash());
+
 app.get("/", (req, res) => {
   res.render("layout");
 });
+app.get("/settings", (req, res) => {
+  res.render("settings");
+});
 
 app.listen(port, () => {
+  //tells express to listen for requests
   console.log(`listening to requests on http://localhost:${port}...`);
 });
